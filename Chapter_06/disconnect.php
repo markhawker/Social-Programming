d05d65838ce7edb9783fec7680f257a18c89a6ae
markhawker <thebubblejungle@gmail.com>
2010-02-11 18:42:04 +0000

    Added disconnect redirection.

--- Chapter_06/disconnect.php
+++ Chapter_06/disconnect.php
@@ -6,8 +6,8 @@ include 'facebook-platform/php/facebook.php';
 $facebook = new Facebook(API_KEY, SECRET);
 $user = $facebook->get_loggedin_user();
 
-$logout = $facebook->api_client->auth_revokeAuthorization();
+$disconnect = $facebook->api_client->auth_revokeAuthorization();
 
-echo $logout;
+header('Location: http://socprog.thebubblejungle.com/facebook/index.php?disconnect='.$disconnect);
 
 ?>
