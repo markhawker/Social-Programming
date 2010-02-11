d787bc29327c7a20424b2641f965e3baff98e656
markhawker <thebubblejungle@gmail.com>
2010-02-11 19:24:43 +0000

    Removed disconnect redirection.

--- Chapter_06/disconnect.php
+++ Chapter_06/disconnect.php
@@ -8,6 +8,17 @@ $user = $facebook->get_loggedin_user();
 
 $disconnect = $facebook->api_client->auth_revokeAuthorization();
 
-header('Location: http://socprog.thebubblejungle.com/facebook/index.php?disconnect='.$disconnect);
-
 ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml">
+<head>
+ <title>Test Tube</title>
+</head>
+<body>
+ <h1>User Disconnected</h1>
+ <p>Status: <?php echo $disconnect; ?></p>
+ <p><a href="index.php">Back Home</a></p>
+</body>
+</html>
