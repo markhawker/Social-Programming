dc2d914a3d24642c0381564b02d0fa1c5cba7097
markhawker <thebubblejungle@gmail.com>
2010-02-26 19:17:22 +0000

    Beginning of Chapter 12 and minor updates to configuration files.

--- Chapter_06/config.php
+++ Chapter_06/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('APP_ID', 'INSERT APP ID HERE');
-define('API_KEY', 'INSERT API KEY HERE');
-define('SECRET', 'INSERT APP SECRET HERE');
+define('APP_ID', 'INSERT APP ID HERE'); // Edit Me
+define('API_KEY', 'INSERT API KEY HERE'); // Edit Me
+define('SECRET', 'INSERT APP SECRET HERE'); // Edit Me
 
 ?>
--- Chapter_08/config.php
+++ Chapter_08/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define('APP_ID', 'INSERT APP ID HERE');
-define('API_KEY', 'INSERT API KEY HERE');
-define('SECRET', 'INSERT APP SECRET HERE');
+define('APP_ID', 'INSERT APP ID HERE'); // Edit Me
+define('API_KEY', 'INSERT API KEY HERE'); // Edit Me
+define('SECRET', 'INSERT APP SECRET HERE'); // Edit Me
 define('CANVAS_PAGE_URL', 'INSERT CANVAS PAGE URL HERE'); // Excluding Traling Slash
 define('CANVAS_CALLBACK_URL', 'INSERT CANVAS CALLBACK URL HERE'); // Excluding Trailing Slash
 
