71aca9f3a925271e2b518742249e59d267f44899
markhawker <thebubblejungle@gmail.com>
2010-03-30 18:10:35 +0100

    Removing files to refresh structure.

--- Chapter_06/friendlinking.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function hash_email($email_address) {
-  $normalized_email_address = strtolower(trim($email_address));
-  return sprintf("%u", crc32($normalized_email_address)).'_'.md5($normalized_email_address);
-}
-
-?>
